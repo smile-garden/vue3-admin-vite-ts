8389bd7ca83f4a6bfe534ede38b5c3d699be38ec
DESKTOP-V1BF6TM\One smile <1539027251@qq.com>
2022-07-07 10:25:43 +0800

    'exception'

--- src/views/exception/NoFoundPage.vue
+++ src/views/exception/NoFoundPage.vue
@@ -1,10 +1,21 @@
 <template>
-  <div>404 page</div>
+  <a-result
+    status="404"
+    title="404"
+    sub-title="Sorry, the page you visited does not exist.">
+    <template #extra>
+      <a-button
+        type="primary"
+        @click="goHome">Back Home</a-button>
+    </template>
+  </a-result>
 </template>
 
 <script setup lang='ts'>
+import { useRouter } from 'vue-router';
 
+const router = useRouter();
+const goHome = () => {
+  router.push('/');
+};
 </script>
-
-<style lang='less' scoped>
-</style>
